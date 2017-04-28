4043b103d4950415d5d6f2eb98bcab96c9962223
Filip Rogalski <f.rogalski@gmail.com>
2017-04-28 22:51:12 +0200

    remove plays with numbers, optimalize code, new functions, adjustable sizes

--- script/app.js
+++ script/app.js
@@ -2,156 +2,109 @@ document.addEventListener('DOMContentLoaded', function () {
     var board = document.getElementById('board');
     var number = document.getElementById('number');
     var value = document.getElementById('value');
+    var cellSize = parseInt(prompt('set cell size'));
+    var boardSize = parseInt(prompt('set board size (cells per side)'));
+    var cellsNumber = boardSize * boardSize;
+    board.style.width = boardSize * cellSize + 'px';
+    board.style.height = boardSize * cellSize + 'px';
+    var cells = [];
     var initializeBoard = function () {
-        for (var i = 0; i < 100; i++) {
+        for (var i = 0; i < cellsNumber; i++) {
             var cell = document.createElement('div');
+            cell.style.width = cellSize + 'px';
+            cell.style.height = cellSize + 'px';
             board.appendChild(cell);
+            cells.push(cell);
         };
     };
     initializeBoard();
-    var cells = document.querySelectorAll('#board div');
     cells.forEach(function (cell) {
         cell.addEventListener('click', function (event) {
             this.classList.toggle('live');
+            console.log(this.style.width);
         });
     });
+    var ant = null;
     var initializeAnt = function () {
-        var ant = document.createElement('div');
+        ant = document.createElement('div');
         ant.id = 'ant';
         board.appendChild(ant);
-        ant.style.left = '40px';
-        ant.style.top = '40px';
+        ant.style.width = cellSize + 'px';
+        ant.style.height = cellSize + 'px';
+        ant.style.left = Math.floor(boardSize * cellSize / 20) * 10 + 'px';
+        ant.style.top = Math.floor(boardSize * cellSize / 20) * 10 + 'px';
         ant.dataset.direction = 0;
     };
     initializeAnt();
-    var directionTop, directionRight, directionBottom, directionLeft;
-    var topMove = null
-        , leftMove = null;
     var getIndexFromCoordinates = function (array) {
-        return array[1] / 10 * 10 + array[0] / 10;
+        return array[1] / cellSize * boardSize + array[0] / cellSize;
+    };
+    var checkAntOnBoard = function () {
+        return parseInt(ant.style.left) >= 0 && parseInt(ant.style.left) < boardSize * cellSize && parseInt(ant.style.top) >= 0 && parseInt(ant.style.top) < boardSize * cellSize;
     };
     var getAntsPosition = function () {
         return [parseInt(ant.style.left), parseInt(ant.style.top)];
-    };
-    var toggleState = function (array) {
-        var antsPosition = getAntsPosition();
-        var index = getIndexFromCoordinates(antsPosition);
-        cells[index].classList.toggle('live');
     };
     var getBaseCell = function () {
         var antsPosition = getAntsPosition();
         var index = getIndexFromCoordinates(antsPosition);
         return cells[index]
     };
-    var getTopMove = function () {
+    var getTopMove = function (baseCell) {
         var antsDirection = parseInt(ant.dataset.direction);
-        var baseCell = getBaseCell();
-        if (antsDirection % 2 == 0) {
-            return 0;
-        }
-        else if (antsDirection == 1) {
-            if (baseCell.className == 'live') {
-                return 1;
-            }
-            else {
-                return -1;
-            }
-        }
-        else {
-            if (baseCell.className == 'live') {
-                return -1;
-            }
-            else {
-                return 1;
-            }
-        };
+        return (antsDirection % 2 !== 0) * Math.pow((-1), (antsDirection == 1)) * Math.pow((-1), (baseCell.className == 'live'));
     };
-    var getTurn = function () {
-        var baseCell = getBaseCell();
+    var getTurn = function (baseCell) {
         return 2 * (baseCell.className == 'live') - 1;
     };
-    var getLeftMove = function () {
+    var getLeftMove = function (baseCell) {
         var antsDirection = parseInt(ant.dataset.direction);
-        var baseCell = getBaseCell();
-        if (antsDirection % 2 == 1) {
-            return 0;
-        }
-        else if (antsDirection == 0) {
-            if (baseCell.className == 'live') {
-                return 1;
-            }
-            else {
-                return -1;
-            }
-        }
-        else {
-            if (baseCell.className == 'live') {
-                return -1;
-            }
-            else {
-                return 1;
-            }
-        };
+        return (antsDirection % 2 !== 1) * Math.pow((-1), (baseCell.className == 'live')) * Math.pow((-1), (antsDirection == 0));
     };
-     
-    var calculateValue = function(){
-        var cellsValues = [0,0,0,0,0,0,0,0,0,0];
-        for(var j = 0; j < 10; j++) {
-        for (var i = 0; i < 10; i++) {
-            if (cells[j*10+i].className == 'live') {
-                cellsValues[j] += 1;
-            }
-            cellsValues[j].push(cells[j*10+i])
-        }
-    };
-        return cellsValues;
-    };
-    
     var moveAnt = function () {
-        var baseCell = getBaseCell();
-        var antsTopPosition = ant.style.top;
-        var antsLeftPosition = ant.style.left;
-        var topMove = getTopMove()
-            , leftMove = getLeftMove()
-            , turn = getTurn();
-        ant.style.top = (100 + parseInt(antsTopPosition) + 10 * topMove)%100 + 'px';
-        ant.style.left = (100 + parseInt(antsLeftPosition) + 10 * leftMove)%100 + 'px';
+        //Function immediatelly gives final result:
+        //        while (checkAntOnBoard()==1) {
+        var baseCell = getBaseCell()
+            , antsTopPosition = ant.style.top
+            , antsLeftPosition = ant.style.left
+            , topMove = getTopMove(baseCell)
+            , leftMove = getLeftMove(baseCell)
+            , turn = getTurn(baseCell);
+        /*The ant always stays in board:
+            ant.style.top = (boardSize * cellSize + parseInt(antsTopPosition) + cellSize * topMove) % (boardSize * cellSize) + 'px';
+        
+            ant.style.left = (boardSize * cellSize + parseInt(antsLeftPosition) + cellSize * leftMove) % (boardSize * cellSize) + 'px';
+            */
+        //Play stops when ant leaves the board:
+        ant.style.top = parseInt(antsTopPosition) + cellSize * topMove + 'px';
+        ant.style.left = parseInt(antsLeftPosition) + cellSize * leftMove + 'px';
         ant.dataset.direction = (4 + parseInt(ant.dataset.direction) + turn) % 4;
         baseCell.classList.toggle('live');
-        var colValue = calculateColValue();
-        number.innerHTML = colValue;
+        //        }
     };
-    
     var z = null;
     var play = document.getElementById('start');
     var pause = document.getElementById('pause');
+    var clearBoard = document.getElementById('clearBoard');
+    var resetAnt = document.getElementById('resetAnt');
     play.addEventListener('click', function (event) {
         z = setInterval(function () {
-            moveAnt()
+            if (checkAntOnBoard() == 1) {
+                moveAnt()
+            }
+            else clearInterval(z)
         }, 100);
     });
     pause.addEventListener('click', function () {
         clearInterval(z);
-        
     });
-    var log = [];
-    var calculateColValue = function(){
-        var value = 0;
-        var colValues = [0,0,0,0,0,0,0,0,0,0];
-        for(var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
-            if (cells[i*10+j].className == 'live') {
-                colValues[j] += 1;
-                value += 1;
-                log.push(value);
-            }
-            
-        }
-    };
-        return colValues;
-    };
-    
-    
-    
-    
+    resetAnt.addEventListener('click', function () {
+        board.removeChild(ant);
+        initializeAnt();
+    });
+    clearBoard.addEventListener('click', function () {
+        cells.forEach(function (cell) {
+            cell.classList.remove('live');
+        });
+    });
 });
